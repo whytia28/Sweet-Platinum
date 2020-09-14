8b7afb23130e94f56a619b0ba8cd0b7f728bc8a1
primsaphira <primsaphira@gmail.com>
2020-09-14 20:00:38 +0700

    menambahkan kelas network

---
+++ app/src/main/java/com/example/sweetPlatinum/network/APIClient.kt
@@ -0,0 +1,21 @@
+package com.example.sweetPlatinum.network
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private const val BASE_URL = "https://still-temple-51886.herokuapp.com/"
+
+    private val client = OkHttpClient.Builder().build()
+
+    val apiService : ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        retrofit.create(ApiService::class.java)
+    }
+}
